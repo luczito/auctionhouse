c1497ed94379eba4b1bb09f0e5f1a6287044a27b
LF <lufr@itu.dk>
2022-11-29 14:12:37 +0100

    shoud be done

--- client/client.go
+++ client/client.go
@@ -8,106 +8,155 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 
 	token "github.com/luczito/auctionhouse/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
-var port = flag.Int("port", 5000, "port") //port for the node default 5000
-
-type STATE int32 //state struct to see wether the node has access, wants access or not.
-
-const (
-	RELEASED STATE = iota
-	HELD
-	REQUESTED
-)
-
-// node struct.
 type Client struct {
-	token.UnimplementedAuctionServer
-	id       int32 //port
-	clients  map[int32]token.AuctionClient
-	server   token.AuctionServer
-	ctx      context.Context
-	queue    []int32
-	state    STATE
-	requests int
+	token.UnimplementedClientServer
+	address       string
+	primaryClient token.ManagerClient
+	connection    *grpc.ClientConn
+	ctx           context.Context
+}
+
+func (c *Client) HeartBeat(ctx context.Context, prim *token.Primary) (*token.Reply, error) {
+	var addr string
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		addr = md.Get("address")[0]
+	}
+
+	log.Printf("Recieved heartbeat from primary %v", addr)
+
+	if addr != c.address {
+		c.address = addr
+
+		conn, err := grpc.DialContext(ctx, addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("Did not connect %v", err)
+		}
+
+		c.connection = conn
+		c.primaryClient = token.NewManagerClient(conn)
+	}
+
+	return &token.Reply{}, nil
 }
 
 func main() {
-	f, err := os.OpenFile("log.server", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	address := flag.String("primary", "127.0.0.1:5000", "primary ip")
+	startPort := flag.Int("port", 5000, "port")
+
+	flag.Parse()
+
+	var port = int32(*startPort)
+
+	ip := fmt.Sprintf("127.0.0.1:%d", port)
+
+	ctx, _ := context.WithCancel(context.Background())
+	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("address", ip))
+
+	//setup logger for client
+	log_, err := os.OpenFile(fmt.Sprintf("client-log-%s.txt", ip), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("error opening file: %v\n", err)
+		log.Fatalf("error opening file: %v", err)
 	}
-	defer f.Close()
-	log.SetOutput(f)
+	defer log_.Close()
 
-	flag.Parse() //set port with -port in the commandline when running the program
+	log.SetOutput(log_)
 
-	ctx_, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var port = int32(*port)
-
-	//create a node for this proccess
-	n := &Client{
-		id:       port,
-		clients:  make(map[int32]token.AuctionClient),
-		queue:    make([]int32, 0),
-		ctx:      ctx_,
-		state:    RELEASED,
-		requests: 0,
-	}
-
-	//creates listener on port
-	list, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	//dial up the primary manager
+	conn, err := grpc.DialContext(ctx, *address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("Failed to listen on port: %v\n", err)
+		log.Fatalf("Failed to connect to client: %v", err)
 	}
 
-	log.Printf("Node created on port: %d\n", n.id)
-	fmt.Printf("Node created on port %v\n", n.id)
+	c := &Client{
+		address:       *address,
+		primaryClient: token.NewManagerClient(conn),
+		connection:    conn,
+		ctx:           ctx,
+	}
+
+	list, err := net.Listen("tcp", ip)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
 	grpcServer := grpc.NewServer()
-	token.RegisterAuctionServer(grpcServer, n)
+	token.RegisterClientServer(grpcServer, c)
 
-	//serve on the listener
 	go func() {
+		log.Printf("\nlistening on %s\n", ip)
+		fmt.Printf("\nlistening on %s\n", ip)
+
 		if err := grpcServer.Serve(list); err != nil {
-			log.Fatalf("failed to server %v\n", err)
+			log.Fatalf("failed to listen on: %v", err)
 		}
 	}()
 
-	//for loop to dial all other nodes in the network, if this loop is increased the number of nodes in the network is aswell
-	for i := 0; i < 6; i++ {
-		nodePort := int32(5000 + i)
-
-		if nodePort == n.id {
-			continue
-		}
-
-		var conn *grpc.ClientConn
-		log.Printf("Trying to dial: %v\n", nodePort)
-		conn, err := grpc.Dial(fmt.Sprintf(":%v", nodePort), grpc.WithInsecure(), grpc.WithBlock())
-
-		if err != nil {
-			log.Fatalf("Could not connect: %v\n", err)
-		}
-
-		defer conn.Close()
-		log.Printf("Succes connecting to: %v\n", nodePort)
-		c := token.NewAuctionClient(conn)
-		n.clients[nodePort] = c
-	}
-
-	log.Printf("%v is connected to %v other nodes\n", n.id, len(n.clients))
-	fmt.Printf("%v is connected to %v other nodes\n", n.id, len(n.clients))
+	fmt.Println("\"bid <price>\" - to bid on an item")
+	fmt.Println("\"result\" - to get the result of the auction")
+	fmt.Println("\"exit\" - to exit the program")
 
 	scanner := bufio.NewScanner(os.Stdin)
 
-	//scanner that requests access from the given node when something is written in the terminal.
+	fmt.Println("Enter a command")
 	for scanner.Scan() {
+		//read in command
+		command := scanner.Text()
 
+		//split command into args
+		args := strings.Split(command, " ")
+
+		//switch on command
+		switch args[0] {
+		case "bid":
+			bid, err := strconv.Atoi(args[1])
+			if err != nil {
+				log.Println("Invalid bid price")
+				fmt.Println("Invalid bid price")
+			}
+
+			response, err := c.primaryClient.Bid(ctx, &token.Amount{Value: int32(bid)})
+			if err != nil {
+				log.Println("Error sending bid")
+				fmt.Println("Error sending bid")
+			}
+
+			if response.Status == token.Status_SUCCESS {
+				log.Printf("Your %d bid was successful\n", bid)
+				fmt.Printf("Your %d bid was successful\n", bid)
+			} else {
+				log.Println("Bid unsuccessful - bid was too low")
+				fmt.Println("Bid unsuccessful - bid was too low")
+			}
+
+		case "result":
+			response, err := c.primaryClient.Result(ctx, &token.Void{})
+			if err != nil {
+				log.Println("Error getting result")
+			}
+
+			switch v := response.Outcome.(type) {
+			case *token.Outcome_AuctionResult:
+				log.Printf("Auction is over! The winning bid was %d and made by %s\n", v.AuctionResult.Amount, v.AuctionResult.Id)
+				fmt.Printf("Auction is over! The winning bid was %d and made by %s\n", v.AuctionResult.Amount, v.AuctionResult.Id)
+			case *token.Outcome_CurrentBid:
+				log.Printf("Auction is still ongoing, %d seconds remaining. Currently, the highest bid is %d\n", response.TimeRemaining, v.CurrentBid.Amount)
+				fmt.Printf("Auction is still ongoing, %d seconds remaining. Currently, the highest bid is %d\n", response.TimeRemaining, v.CurrentBid.Amount)
+			}
+		case "exit":
+			os.Exit(0)
+		}
+
+		fmt.Print("Enter command: ")
+		log.Print("Enter command: ")
 	}
 }
